51be5bbf22464737136fe6c91406777c3a54b573
Oleksandr Hnutov <sanerhp.mail@gmail.com>
2025-11-06 17:39:30 +0200

    10 11 + fixes

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(cnt: u32) -> u32 {
+    if cnt > 40 {
+        cnt
+    } else {
+        cnt * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .iter()
+            .map(|(s, cmd)| {
+                match cmd {
+                    Command::Uppercase => s.to_uppercase(),
+                    Command::Trim => s.trim().to_owned(),
+                    Command::Append(i) => {
+                        let mut s = s.to_owned();
+
+                        for _ in 0..*i {
+                            s.push_str("bar");
+                        }
+
+                        s
+                    }
+                }
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -37,7 +56,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
